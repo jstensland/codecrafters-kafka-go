0a9735b5e2ca6b2294ef740d214c8c620f4e9663
Jared Stensland <jared.stensland@gmail.com>
2025-04-17 22:13:02 -0700

    move server to its own package

--- app/main.go
+++ app/main.go
@@ -1,39 +1,10 @@
-// Package main implements a simple Kafka broker that handles API versions requests.
+// Package main provides the entry point for the Kafka broker application.
 package main
 
 import (
-	"log"
-	"net"
-	"os"
-
-	"github.com/codecrafters-io/kafka-starter-go/app/handlers"
+	"github.com/codecrafters-io/kafka-starter-go/app/server"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	log.Println("Logs from your program will appear here!")
-
-	// Listen on both IPv4 and IPv6
-	// #nosec G102 -- This is intentional for the challenge
-	l, err := net.Listen("tcp", ":9092")
-	if err != nil {
-		log.Println("Failed to bind to port 9092")
-		os.Exit(1)
-	}
-	defer func() {
-		if err := l.Close(); err != nil {
-			log.Printf("Error closing listener: %v", err)
-		}
-	}() // Ensure listener is closed when main exits
-
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			log.Printf("Error accepting connection: %v", err)
-			continue // Continue listening for other connections
-		}
-
-		// Handle connection in a new goroutine to allow concurrent connections
-		go handlers.HandleConnection(conn, handlers.ConnectionReadTimeout)
-	}
+	server.Serve()
 }
